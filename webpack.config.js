07e8ac9ce097057d1a6d3187a825420d97c6ab9a
Jacob Sebastian <jacobcsebastian@ozforex.com.au>
2016-01-28 22:41:02 +1100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+
+//In Node.js, __dirname is always the directory in which the currently executing script resides
+
+var BUILD_DIR = path.resolve(__dirname, './public/');
+var APP_DIR = path.resolve(__dirname, 'src/');
+
+var config = {
+    entry:[
+        'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        APP_DIR + '/index.js'
+        ],
+    output: {
+        path: BUILD_DIR,
+        filename: 'app.js'
+
+    },
+    module: {
+        loaders:[{
+            test : /\.jsx?/,
+
+            loaders:['react-hot','babel'],
+            include :APP_DIR
+        }]
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin()
+    ]
+
+};
+
+module.exports = config;
